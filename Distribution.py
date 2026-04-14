68648821717b43456e0a1b99daaa88d88250f794
kuroi042 <bouderraoui45@gmail.com>
2026-04-14 01:52:05 +0100

    pie chart

--- Distribution.py
+++ Distribution.py
@@ -1,12 +1,38 @@
 import os
 import pandas as pd
 import numpy as np
-count  = 0
-for root, dirs, files in os.walk("Apple"):
-        count+= len(files)
-        if len(files) == 0:
-            continue
-        print("Folders inside" ,root , count)
-        
+import sys
+import matplotlib.pyplot as plt
+def createBar(data : dict):
+    df = pd.DataFrame(list(data.items()), columns=["Class", "Count"])
+    print(df.head())
+    plt.pie(df.)
+    plt.bar(df["Class"],df["Count"], color='skyblue')
+    plt.show()  
 
+
+def ft_load(text :str):
+    data = {}
+    for root, dirs, files in os.walk("Apple"):
+            if len(files) == 0:
+                continue
+            value =  len(files)
+            key= root.split('/')[1]
+            # print("Folders inside" ,root , count)
+            # print(key.split('/')[1] ,":",value)
+            data[key]=value
+            # print(key , value)
+    createBar(data)
+
+def main():
+    try:
+          assert len(sys.argv) ==2 ,"the arguments are bad"
+    except AssertionError as e:
+        print(f"AssertionError: {e}")
+    path = str(sys.argv)
+    ft_load(path)
+
+
+if __name__ ==  "__main__":
+     main()
  
