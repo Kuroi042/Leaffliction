4173e7e1d3a3e60cac29f26dd79ee12bbb421dd0
kuroi042 <bouderraoui45@gmail.com>
2026-04-15 16:45:27 +0100

    rotate + flip

--- Augmentation.py
+++ Augmentation.py
@@ -6,16 +6,29 @@ import random as rnd
 from pathlib import Path
 cv2.namedWindow('img', cv2.WINDOW_NORMAL)
 
+def ft_rotate(path:str):
+    img  =  cv2.imread(path)
+    (h, w) = img.shape[:2]
+    center = (w // 2, h // 2)
+    angle = 18 
+    scale = 1.0
+    M = cv2.getRotationMatrix2D(center, angle, scale)
+    rotated_image = cv2.warpAffine(img, M, (w, h), flags=cv2.INTER_LINEAR, borderMode=cv2.BORDER_REFLECT)
+    cv2.imshow('Rotated Leaf', rotated_image)    
+    cv2.waitKey(0)
+
+def ft_flip(path:str):
+    img  =  cv2.imread(path)
+    img_flip =  cv2.flip(img,rnd.randrange(-1,1)) # range between -1->1
+    cv2.imshow('imgflip',img_flip)
+    cv2.waitKey(0)
+
 def operate(path:str):
 
     img  = cv2.imread(path)
-    #range 15-20 degrees
-    # img_flip =  cv2.flip(img,rnd.randrange(-1,1)) # range between -1->1
-    # cv2.imshow('imgflip',img_flip)
-    img_rotate =  cv2.rotate(img)
-    
     cv2.imshow('img',img)
-    cv2.waitKey(0)
+    ft_rotate(path)
+    ft_flip(path)
     
 
 
