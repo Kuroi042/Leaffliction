fbb559f4ef8e72996c16836108730d9f2b4735fd
kuroi042 <bouderraoui45@gmail.com>
2026-04-17 22:19:59 +0100

    not confused anymore , shear en poche

--- Augmentation.py
+++ Augmentation.py
@@ -28,37 +28,24 @@ def ft_flip(path:str):
     img_flip =  cv2.flip(img,rnd.randrange(-1,1)) # range between -1->1
     cv2.imshow('imgflip',img_flip)
     cv2.waitKey(0)
-# def ft_blur(path:str):
-#     img =  cv2.imread(path)
-#     img_blur =  cv2.blur(img,(5,5))
-#     cv2.imshow('img_blur' , img_blur)
-#     cv2.waitKey(0)
+
+
 def ft_scaling(path:str):
     img =  cv2.imread(path)
     resized_img = cv2.resize(img, ((224, 224)))
     cv2.imshow('img_scale',resized_img)
     cv2.waitKey(0)
-# def skew(path:str):
 
 def ft_shear(path:str):
     img = cv2.imread(path)
     print(img.shape)
     h,w = img.shape[:2]
-    shear_factor = 0.5
-    # flipped = cv2.flip(img, 0)
-
-    M = np.float32([[1, shear_factor, 0],
+    shear_factor = 0.3
+    Mh = np.float32([[1, shear_factor, 0],
                     [0, 1, 0]])
-    # [1, k, 0] = x₁ = 1*x + k*y + 0 | x1 =x
-    # [0, 1, 0] =  y₁ = 0*x + 1*y + 0  | y1 =y
-    
-    # (x, y) = (10, 0) |  k = 0.5
-    # x1 = 1 * 10 + 0.5*0 + 0 = 10.5
-    # y1 = 0 + 1*0.5 +0  = 0.5
-    new_w = int(w + h * shear_factor) 
-    shear = cv2.warpAffine(img, M, (new_w, h))
-    # result = cv2.flip(shear, 0)
-
+    Mv = np.float32([[1, 0, 0],
+                    [0.3 ,1, 0]])
+    shear = cv2.warpAffine(img,rnd.choice([Mv, Mh]) , (w, h))
     cv2.imshow('shear',shear)
     cv2.waitKey(0)
 def operate(path:str):
@@ -67,11 +54,11 @@ def operate(path:str):
     cv2.imshow('img',img)
     # ft_rotate(path)
     # ft_flip(path)
-    ft_shear(path)
+    # ft_shear(path)
     # ft_blur(path)
     # ft_contrast(path)
     # ft_scaling(path)
-    
+    ft_distort(path)
 
 def main():
     try:
