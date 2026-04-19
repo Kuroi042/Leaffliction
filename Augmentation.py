376e013b11435b94ac427c79151c0f10a5887d39
kuroi042 <bouderraoui45@gmail.com>
2026-04-19 11:04:00 +0100

    skew done

--- Augmentation.py
+++ Augmentation.py
@@ -16,24 +16,20 @@ def ft_rotate(path:str):
     rotated_image = cv2.warpAffine(img, M, (w, h), flags=cv2.INTER_LINEAR, borderMode=cv2.BORDER_REFLECT)
     cv2.imshow('Rotated Leaf', rotated_image)    
     cv2.waitKey(0)
-# def ft_contrast(path:str):
-#     img  =  cv2.imread(path)
-#     alpha =  np.random.uniform(1.1, 2.5)
-#     beta =  np.random.randint(-20, 20)
-#     img_contrast = cv2.convertScaleAbs(img , alpha=alpha, beta= beta)
-#     cv2.imshow('img_contrast', img_contrast)
-#     cv2.waitKey(0)
 def ft_flip(path:str):
     img  =  cv2.imread(path)
     img_flip =  cv2.flip(img,rnd.randrange(-1,1)) # range between -1->1
     cv2.imshow('imgflip',img_flip)
     cv2.waitKey(0)
+
 def skew(path:str):
     image = cv2.imread(path)
 
     h, w = image.shape[:2]
-    offset = rnd.randrange(10 , 50)
-    print(offset)
+    # how mucht he image will be skewed 10-20 of the width
+    offset = rnd.randint(int(w*0.1),int(w*0.2)) #(10%-20%) of width 
+    # (256, 256, 3)
+    print(image.shape)
     # original 4 corners
     pts1 = np.float32([
         [0, 0],
@@ -43,6 +39,7 @@ def skew(path:str):
     ])
 
     # new 4 corners → top shifted right
+    # tells OpenCV where to move the pixels
     pts2 = np.float32([
         [offset, 0],
         [w + offset, 0],
@@ -51,9 +48,11 @@ def skew(path:str):
     ])
 
     M = cv2.getPerspectiveTransform(pts1, pts2)
-    result = cv2.warpPerspective(image, M, (w + offset, h))
-
- 
+    # add the ofset on the canvas to show the skew 
+    # try h+ofset for hieght 
+    result = cv2.warpPerspective(image, M, (w+offset, h))
+    # print(result.shape)
+    # (256, 296, 3)
     cv2.imshow('Skewed', result)
     cv2.waitKey(0)
 
