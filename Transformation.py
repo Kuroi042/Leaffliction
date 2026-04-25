36ca8671722aa42f1f24d72fd043743226b285d9
kuroi042 <bouderraoui45@gmail.com>
2026-04-26 00:36:48 +0100

    test.png

--- Transformation.py
+++ Transformation.py
@@ -8,23 +8,21 @@ import cv2
 
 def    Execute_filter(Path):
     import matplotlib
-    # pcv.params.debug = "plot" 
     img, _, _ = pcv.readimage(filename=Path, mode="native")
-    
-    # colors = pcv.visualize.colorspaces(rgb_img=img, original_img=False)
-    # plt.imshow(colors, cmap='gray')
-    # a = pcv.rgb2gray_lab(img, 'a')
-    # blur = pcv.gaussian_blur(img=a, ksize=(5,5))
-    # # bin_gauss1 = pcv.threshold.gaussian(gray_img=blur, ksize=31, offset=7,object_type='dark')
-    # thresh = pcv.threshold.otsu(
-    # gray_img=blur,
-    # object_type='dark'
-# )
-    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-    blur = cv2.GaussianBlur(hsv,(5,5),0)
-    plt.imshow(blur, cmap='gray')
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
+    # step 2: apply gaussian blur
+    blur = cv2.GaussianBlur(gray, (3, 3), 0)
+    _, thresh = cv2.threshold(
+        blur,
+        0,      # threshold value
+        255,    # max value
+        cv2.THRESH_BINARY_INV +cv2.THRESH_OTSU
+    )
+
+    plt.imshow( thresh, cmap='gray')
     plt.show()
+
 def main():
     try:
         assert len(sys.argv) == 2 , "argument are bad"
