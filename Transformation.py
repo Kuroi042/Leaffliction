eb8fe85b1ca73a2c057e4ad9c9f0cc39a944bc02
kuroi042 <bouderraoui45@gmail.com>
2026-05-13 13:10:19 +0100

    rgb , blur , mask

--- Transformation.py
+++ Transformation.py
@@ -19,6 +19,7 @@ class Transforme:
         self.debug =  tools.debug
         self.outdir = tools.outdir
         self.rgb =   None
+        # self.tresh = None
         self.blur =  None
         self.mask =  None
         pcv.params.debug = self.debug
@@ -29,19 +30,72 @@ class Transforme:
         self.rgb,_,_=pcv.readimage(filename=self.path, mode="native")
         if self.rgb is not None:
             print("rgb is here successfully ! ")
-        return self.rgb
-def display(self):
-    if self.rgb is not None:
-        plt.imshow(
-            cv2.cvtColor(self.rgb, cv2.COLOR_BGR2RGB)
-        )
-        plt.title('Original')
-        plt.axis('off')
+        plt.imshow(self.rgb)
         plt.show()
+        
+        return self.rgb
+    def gaussian_blur(self):
+        if self.rgb is None:
+            self.read_orginal()
+        gray = pcv.rgb2gray_hsv(rgb_img=self.rgb, channel="s")
+        thresh = pcv.threshold.binary(gray_img=gray,
+                 threshold=60,  object_type="light")
+        self.blur = pcv.gaussian_blur(
+            img=thresh,
+            ksize=(5, 5),
+            sigma_x=0,
+            sigma_y=None
+        )
+
+    #     _, self.blur = cv2.threshold(
+    #     blurred,
+    #     0,
+    #     255,
+    #     cv2.THRESH_BINARY + cv2.THRESH_OTSU
+    # )
+        print("blur is here")
+        plt.imshow(self.blur,cmap="gray")
+        plt.show()
+        return self.blur
+    
+    def mask_filter(self):
+        if self.blur is None:
+            self.gaussian_blur()
+        thresh_blur = pcv.threshold.binary(
+            gray_img=self.blur,
+            threshold=127,
+            object_type="light"
+        )
+
+        mask1 = pcv.fill(
+            bin_img=thresh_blur,
+            size=200
+        )
+        self.mask = pcv.apply_mask(
+        img=self.rgb,
+        mask=mask1,
+        mask_color="white"
+    )
+        plt.imshow(self.mask)
+        plt.show()
+        return self.mask
+
+
+
+        
+# def display(self):
+#     if self.rgb is not None:
+#         plt.imshow(
+#             cv2.cvtColor(self.rgb, cv2.COLOR_BGR2RGB)
+#         )
+#         plt.title('Original')
+#         plt.axis('off')
+#         plt.show()
 def Execute_filter(tools:handytools):
     leaf  =  Transforme(tools)
     leaf.read_orginal()
-
+    leaf.gaussian_blur()
+    leaf.mask_filter()
 
 
 def main():
