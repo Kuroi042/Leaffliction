0b4499ded287677707ba4060a6faec02be18d285
kuroi042 <bouderraoui45@gmail.com>
2026-06-05 14:29:54 +0100

    clean the main files for new collaborator nabil.r

--- test.py
+++
@@ -1,7 +0,0 @@
-import cv2
-import os
-import plantcv as pcv
-img = cv2.imread(os.path.join('Apple/Apple_Black_rot/image (1).JPG'))
-blur =  cv2.blur(img, ksize=(7,7) )
-cv2.imshow("sdasd",blur)
-cv2.waitKey(0)
